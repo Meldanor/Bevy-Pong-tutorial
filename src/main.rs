d8e0f634056e5d98207ca61e627be2a8b81dac42
Kilian Gärtner <herr.kilian.gaertner@gmail.com>
2025-01-04 20:59:41 +0100

    add: hello world example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::{
+    app::{App, Startup},
+    DefaultPlugins,
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, hello_world)
+        .run();
+}
+
+fn hello_world() {
+    println!("Hello world!");
 }
