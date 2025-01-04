e0b7bb50d9c2e4edfe48e3097134e300b52dc323
Kilian Gärtner <herr.kilian.gaertner@gmail.com>
2025-01-04 23:37:49 +0100

    add: presentation of ball

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,53 @@
 use bevy::{
-    app::{App, Startup},
+    app::{App, Startup, Update},
+    math::Vec2,
+    prelude::{Bundle, Camera2d, Commands, Component, Query, Transform},
     DefaultPlugins,
 };
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, hello_world)
+        .add_systems(Startup, (spawn_ball, spawn_camera))
+        .add_systems(Update, (project_positions))
         .run();
 }
 
-fn hello_world() {
-    println!("Hello world!");
+#[derive(Component)]
+struct Position(Vec2);
+
+#[derive(Component)]
+struct Ball;
+
+#[derive(Bundle)]
+struct BallBundle {
+    ball: Ball,
+    position: Position,
+}
+
+impl BallBundle {
+    fn new() -> Self {
+        Self {
+            ball: Ball,
+            position: Position(Vec2::new(0., 0.)),
+        }
+    }
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_empty().insert(Camera2d::default());
+}
+
+fn spawn_ball(mut commands: Commands) {
+    println!("Spawning ball...");
+    commands
+        .spawn_empty()
+        .insert(Transform::default())
+        .insert(BallBundle::new());
+}
+
+fn project_positions(mut positionables: Query<(&mut Transform, &Position)>) {
+    for (mut transform, position) in &mut positionables {
+        transform.translation = position.0.extend(0.)
+    }
 }
