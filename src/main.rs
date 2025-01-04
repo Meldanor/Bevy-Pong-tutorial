49ff742ccbfe139f647b43b404c2fc570f3b01b2
Kilian Gärtner <herr.kilian.gaertner@gmail.com>
2025-01-05 00:36:02 +0100

    feat: basic collision handling

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use bevy::{
     app::{App, Startup, Update},
     asset::Assets,
     color::palettes::css::{BLUE, RED},
-    math::Vec2,
+    math::{
+        bounding::{Aabb2d, BoundingCircle, IntersectsVolume},
+        Vec2,
+    },
     prelude::{
         Bundle, Camera2d, Circle, Commands, Component, IntoSystemConfigs, Mesh, Mesh2d, Query,
-        Rectangle, ResMut, Transform, With,
+        Rectangle, ResMut, Transform, With, Without,
     },
     sprite::{ColorMaterial, MeshMaterial2d},
     DefaultPlugins,
@@ -15,7 +18,10 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_ball, spawn_paddles, spawn_camera))
-        .add_systems(Update, (move_ball, project_positions).chain())
+        .add_systems(
+            Update,
+            (move_ball, handle_collisions, project_positions).chain(),
+        )
         .run();
 }
 
@@ -25,6 +31,11 @@ fn spawn_camera(mut commands: Commands) {
 
 #[derive(Component)]
 struct Position(Vec2);
+#[derive(Component)]
+struct Velocity(Vec2);
+
+#[derive(Component)]
+struct Shape(Vec2);
 
 #[derive(Component)]
 struct Ball;
@@ -32,13 +43,20 @@ struct Ball;
 #[derive(Bundle)]
 struct BallBundle {
     ball: Ball,
+    shape: Shape,
+    velocity: Velocity,
     position: Position,
 }
 
 impl BallBundle {
-    fn new() -> Self {
+    fn new(velocity: Vec2) -> Self {
         Self {
             ball: Ball,
+            shape: Shape(Vec2 {
+                x: BALL_SIZE,
+                y: BALL_SIZE,
+            }),
+            velocity: Velocity(velocity),
             position: Position(Vec2::new(0., 0.)),
         }
     }
@@ -56,7 +74,11 @@ fn spawn_ball(
     let mesh = meshes.add(Circle::new(BALL_SIZE));
     let material = materials.add(ColorMaterial::from_color(RED));
 
-    commands.spawn((BallBundle::new(), Mesh2d(mesh), MeshMaterial2d(material)));
+    commands.spawn((
+        BallBundle::new(Vec2::new(1., 0.)),
+        Mesh2d(mesh),
+        MeshMaterial2d(material),
+    ));
 }
 
 fn project_positions(mut positionables: Query<(&mut Transform, &Position)>) {
@@ -67,9 +89,9 @@ fn project_positions(mut positionables: Query<(&mut Transform, &Position)>) {
 
 const BALL_SPEED: f32 = 1.0;
 
-fn move_ball(mut ball: Query<&mut Position, With<Ball>>) {
-    if let Ok(mut position) = ball.get_single_mut() {
-        position.0.x += BALL_SPEED;
+fn move_ball(mut ball: Query<(&mut Position, &Velocity), With<Ball>>) {
+    if let Ok((mut position, velocity)) = ball.get_single_mut() {
+        position.0 += velocity.0 * BALL_SPEED;
     }
 }
 
@@ -79,14 +101,21 @@ struct Paddle;
 #[derive(Bundle)]
 struct PaddleBundle {
     paddle: Paddle,
+    shape: Shape,
     position: Position,
+    velocity: Velocity,
 }
 
 impl PaddleBundle {
     fn new(x: f32, y: f32) -> Self {
         Self {
             paddle: Paddle,
+            shape: Shape(Vec2 {
+                x: PADDLE_WIDTH,
+                y: PADDLE_HEIGHT,
+            }),
             position: Position(Vec2::new(x, y)),
+            velocity: Velocity(Vec2 { x: 0., y: 0. }),
         }
     }
 }
@@ -105,8 +134,58 @@ fn spawn_paddles(
     let material = materials.add(ColorMaterial::from_color(BLUE));
 
     commands.spawn((
-        PaddleBundle::new(20., -25.),
+        PaddleBundle::new(200., -25.),
         Mesh2d(mesh),
         MeshMaterial2d(material),
     ));
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Collision {
+    Left,
+    Right,
+    Top,
+    Bottom,
+}
+
+fn collide_with_side(ball: BoundingCircle, wall: Aabb2d) -> Option<Collision> {
+    if !ball.intersects(&wall) {
+        return None;
+    }
+    let closest_point = wall.closest_point(ball.center);
+    let offset = ball.center - closest_point;
+
+    let side = if offset.x.abs() > offset.y.abs() {
+        if offset.x < 0. {
+            Collision::Left
+        } else {
+            Collision::Right
+        }
+    } else if offset.y > 0. {
+        Collision::Top
+    } else {
+        Collision::Bottom
+    };
+    Some(side)
+}
+
+fn handle_collisions(
+    mut ball: Query<(&mut Velocity, &Position, &Shape), With<Ball>>,
+    other_things: Query<(&Position, &Shape), Without<Ball>>,
+) {
+    if let Ok((mut ball_velocity, ball_position, ball_shape)) = ball.get_single_mut() {
+        for (position, shape) in &other_things {
+            if let Some(collision) = collide_with_side(
+                BoundingCircle::new(ball_position.0, ball_shape.0.x),
+                Aabb2d::new(position.0, shape.0 / 2.),
+            ) {
+                match collision {
+                    Collision::Left => ball_velocity.0.x *= -1.,
+                    Collision::Right => ball_velocity.0.x *= -1.,
+                    Collision::Top => ball_velocity.0.y *= -1.,
+                    Collision::Bottom => ball_velocity.0.y *= -1.,
+                }
+            }
+        }
+    }
 }
