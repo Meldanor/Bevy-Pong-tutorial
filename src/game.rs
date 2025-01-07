df40eab7dcd5fd856544966e5658b97ee6fdeb69
Kilian Gärtner <herr.kilian.gaertner@gmail.com>
2025-01-07 21:03:06 +0100

    feat: handle collisions through rapier

--- src/game.rs
+++ src/game.rs
@@ -1,13 +1,11 @@
 use bevy::{
     audio::Volume,
     color::palettes::css::{BLACK, BLUE, GREEN, RED},
-    math::bounding::{Aabb2d, BoundingCircle, IntersectsVolume},
     prelude::*,
 };
 use bevy_rapier2d::{
     plugin::{NoUserData, RapierPhysicsPlugin},
-    prelude::Collider,
-    render::RapierDebugRenderPlugin,
+    prelude::{ActiveEvents, Collider, CollisionEvent, CollisionGroups, Group, RigidBody},
 };
 
 use crate::positions::Position;
@@ -19,7 +17,6 @@ impl Plugin for PongPlugin {
             .init_resource::<PongSounds>()
             .add_event::<Scored>()
             .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1.))
-            .add_plugins(RapierDebugRenderPlugin::default())
             .add_systems(
                 Startup,
                 (
@@ -28,7 +25,8 @@ impl Plugin for PongPlugin {
                     spawn_gutters,
                     spawn_scoreboards,
                     load_assets,
-                ),
+                )
+                    .chain(),
             )
             .add_systems(Update, (update_scoreboards, handle_player_input).chain())
             .add_systems(
@@ -40,6 +38,7 @@ impl Plugin for PongPlugin {
                     reset_ball,
                     move_ai,
                     move_paddles,
+                    handle_ball_collisions,
                     // handle_collisions,
                 )
                     .chain(),
@@ -68,15 +67,22 @@ struct Ball;
 struct BallBundle {
     ball: Ball,
     collider: Collider,
+    collision_group: CollisionGroups,
+    bla: RigidBody,
     velocity: Velocity,
     position: Position,
 }
+
+const BALL_COLLISION_GROUP: Group = Group::GROUP_1;
+const REFLECT_COLLISION_GROUP: Group = Group::GROUP_2;
 
 impl BallBundle {
     fn new(velocity: Vec2) -> Self {
         Self {
             ball: Ball,
             collider: Collider::ball(BALL_SIZE),
+            collision_group: CollisionGroups::new(BALL_COLLISION_GROUP, REFLECT_COLLISION_GROUP),
+            bla: RigidBody::Dynamic,
             velocity: Velocity(velocity),
             position: Position(Vec2::new(0., 0.)),
         }
@@ -96,9 +102,10 @@ fn spawn_ball(
     let material = materials.add(ColorMaterial::from_color(RED));
 
     commands.spawn((
-        BallBundle::new(Vec2::new(1., 1.)),
+        BallBundle::new(Vec2::new(1., 0.)),
         Mesh2d(mesh),
         MeshMaterial2d(material),
+        ActiveEvents::COLLISION_EVENTS,
     ));
 }
 
@@ -117,6 +124,8 @@ struct Paddle;
 struct PaddleBundle {
     paddle: Paddle,
     collider: Collider,
+    collision_group: CollisionGroups,
+    bla: RigidBody,
     position: Position,
     velocity: Velocity,
 }
@@ -126,7 +135,9 @@ impl PaddleBundle {
         Self {
             paddle: Paddle,
             collider: Collider::cuboid(PADDLE_WIDTH / 2., PADDLE_HEIGHT / 2.),
+            collision_group: CollisionGroups::new(REFLECT_COLLISION_GROUP, BALL_COLLISION_GROUP),
             position: Position(Vec2::new(x, y)),
+            bla: RigidBody::Fixed,
             velocity: Velocity(Vec2 { x: 0., y: 0. }),
         }
     }
@@ -153,8 +164,7 @@ fn spawn_paddles(
 
     let mesh = meshes.add(Rectangle::new(PADDLE_WIDTH, PADDLE_HEIGHT));
     let player_color = materials.add(ColorMaterial::from_color(BLUE));
-    let ai_color = materials.add(ColorMaterial::from_color(GREEN));
-
+    let ai_color: Handle<ColorMaterial> = materials.add(ColorMaterial::from_color(GREEN));
     commands.spawn((
         Player,
         PaddleBundle::new(right_paddle_x, -0.),
@@ -170,63 +180,43 @@ fn spawn_paddles(
     ));
 }
 
-// #[derive(Debug, PartialEq, Eq, Copy, Clone)]
-// enum Collision {
-//     Left,
-//     Right,
-//     Top,
-//     Bottom,
-// }
-
-// fn collide_with_side(ball: BoundingCircle, wall: Aabb2d) -> Option<Collision> {
-//     if !ball.intersects(&wall) {
-//         return None;
-//     }
-//     let closest_point = wall.closest_point(ball.center);
-//     let offset = ball.center - closest_point;
-
-//     let side = if offset.x.abs() > offset.y.abs() {
-//         if offset.x < 0. {
-//             Collision::Left
-//         } else {
-//             Collision::Right
-//         }
-//     } else if offset.y > 0. {
-//         Collision::Top
-//     } else {
-//         Collision::Bottom
-//     };
-//     Some(side)
-// }
-
-// fn handle_collisions(
-//     mut commands: Commands,
-//     mut ball: Query<(&mut Velocity, &Position, &Shape), With<Ball>>,
-//     other_things: Query<(&Position, &Shape), Without<Ball>>,
-//     pong_sounds: Res<PongSounds>,
-// ) {
-//     let (mut ball_velocity, ball_position, ball_shape) = match ball.get_single_mut() {
-//         Ok(tuple) => tuple,
-//         Err(_) => return,
-//     };
-//     for (position, shape) in &other_things {
-//         if let Some(collision) = collide_with_side(
-//             BoundingCircle::new(ball_position.0, ball_shape.0.x),
-//             Aabb2d::new(position.0, shape.0 / 2.),
-//         ) {
-//             commands.spawn((
-//                 AudioPlayer::new(pong_sounds.on_hit.clone_weak()),
-//                 PlaybackSettings::ONCE.with_volume(Volume::new(0.5)),
-//             ));
-//             match collision {
-//                 Collision::Left => ball_velocity.0.x *= -1.,
-//                 Collision::Right => ball_velocity.0.x *= -1.,
-//                 Collision::Top => ball_velocity.0.y *= -1.,
-//                 Collision::Bottom => ball_velocity.0.y *= -1.,
-//             }
-//         }
-//     }
-// }
+fn handle_ball_collisions(
+    mut collision_events: EventReader<CollisionEvent>,
+    mut ball: Query<(&mut Velocity, &Position), With<Ball>>,
+    paddle: Query<&Position, With<Paddle>>,
+    gutter: Query<&Position, With<Gutter>>,
+    mut commands: Commands,
+    pong_sounds: Res<PongSounds>,
+) {
+    for collision_event in collision_events.read() {
+        match collision_event {
+            CollisionEvent::Started(entity_a, entity_b, _flags) => {
+                if let Ok((mut ball_velocity, ball_position)) = ball.get_mut(*entity_b) {
+                    commands.spawn((
+                        AudioPlayer::new(pong_sounds.on_hit.clone_weak()),
+                        PlaybackSettings::ONCE.with_volume(Volume::new(0.5)),
+                    ));
+                    if let Ok(paddle) = paddle.get(*entity_a) {
+                        ball_velocity.0.x = (ball_position.0 - paddle.0).x.signum();
+                    } else if let Ok(gutter) = gutter.get(*entity_a) {
+                        ball_velocity.0.y = (ball_position.0 - gutter.0).y.signum();
+                    }
+                } else if let Ok((mut ball_velocity, ball_position)) = ball.get_mut(*entity_a) {
+                    commands.spawn((
+                        AudioPlayer::new(pong_sounds.on_hit.clone_weak()),
+                        PlaybackSettings::ONCE.with_volume(Volume::new(0.5)),
+                    ));
+                    if let Ok(paddle) = paddle.get(*entity_b) {
+                        ball_velocity.0.x = (ball_position.0 - paddle.0).x.signum();
+                    } else if let Ok(gutter) = gutter.get(*entity_b) {
+                        ball_velocity.0.y = (ball_position.0 - gutter.0).y.signum();
+                    }
+                }
+            }
+            _ => return,
+        }
+    }
+}
 
 #[derive(Component)]
 struct Player;
@@ -255,6 +245,8 @@ struct Gutter;
 struct GutterBundle {
     gutter: Gutter,
     collider: Collider,
+    collision_group: CollisionGroups,
+    bla: RigidBody,
     position: Position,
 }
 
@@ -263,6 +255,8 @@ impl GutterBundle {
         Self {
             gutter: Gutter,
             collider: Collider::cuboid(width / 2., GUTTER_HEIGHT / 2.),
+            collision_group: CollisionGroups::new(REFLECT_COLLISION_GROUP, BALL_COLLISION_GROUP),
+            bla: RigidBody::Fixed,
             position: Position(Vec2::new(x, y)),
         }
     }
